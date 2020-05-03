e5fdde61314e0a5964795f868c9ea8bfca57d9a2
Raquel Santos <marinaluz371@gmail.com>
2020-05-02 19:29:34 -0700

    Initial Commit

---
+++ RestaurantMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantMenu
+//
+//  Created by user170369 on 5/2/20.
+//  Copyright © 2020 user170369. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RestaurantMenuTests/RestaurantMenuTests.swift
@@ -0,0 +1,34 @@
+//
+//  RestaurantMenuTests.swift
+//  RestaurantMenuTests
+//
+//  Created by user170369 on 5/2/20.
+//  Copyright © 2020 user170369. All rights reserved.
+//
+
+import XCTest
+@testable import RestaurantMenu
+
+class RestaurantMenuTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
